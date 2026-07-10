a78a70ffa8d3cc06ccd5e5d81c65bd7dfc3b3d00
ryanporter56 <ryanporter56@gmail.com>
2026-07-10 12:26:53 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int l = 0;
+        int r = 1;
+        while (l < prices.length - 1 && r < prices.length) {
+            if (prices[l] > prices[r]) {
+                l = r;
+                r++;
+            } else if (prices[r] - prices[l] > maxProfit) {
+                maxProfit = prices[r] - prices[l];
+            } else {
+                r++;
+                continue;
+            }
+        }
+        return maxProfit;
+    }
+}
